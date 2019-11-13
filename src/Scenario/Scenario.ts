2f9cb6fc520608de71498c16c29ed3d84a27affc
vspirit803 <lx546488173@gmail.com>
2019-11-13 18:27:44 +0800

    2019-11-13 18:27:43

--- src/Scenario/Scenario.ts
+++ src/Scenario/Scenario.ts
@@ -7,12 +7,26 @@ import { ScenarioSentenceBase } from './ScenarioSentenceBase';
 export class Scenario {
     sentences: Array<ScenarioSentenceBase>;
     index: number;
+    nextIndex: number;
     onComplete: Function;
+    onAction: Function;
     data: { [propName: string]: number | string | boolean };
-    constructor({ onComplete }: { onComplete: Function }) {
+    constructor({ onComplete, onAction }: { onComplete: Function; onAction: Function }) {
         this.sentences = new Array<ScenarioSentenceBase>(0);
         this.index = 0;
+        this.nextIndex = 1;
         this.onComplete = onComplete;
+        this.onAction = onAction;
         this.data = {};
+    }
+
+    next(): void {
+        if (this.nextIndex >= this.sentences.length) {
+            this.onComplete(this.data);
+            return;
+        }
+        this.index = this.nextIndex;
+        this.onAction(this, this.sentences[this.index]);
+        // this.sentences[this.index].action(this);
     }
 }
--- src/Scenario/ScenarioSentenceBase.ts
+++ src/Scenario/ScenarioSentenceBase.ts
@@ -1,9 +1,15 @@
 /**
  * 剧本句子的基类
  */
-
+import { Scenario } from './Scenario';
 export abstract class ScenarioSentenceBase {
     type: string;
+    action(scenario: Scenario): void {
+        /**
+         * todo
+         */
+    }
+
     constructor() {
         this.type = '';
     }
