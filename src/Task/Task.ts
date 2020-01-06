ef5c377357d6392cbd214ecd0a029904d5f9eeb4
vspirit803 <52484564+vspirit803@users.noreply.github.com>
2020-01-06 22:06:15 +0800

    新增: 任务

--- src/Task/Task.ts
+++ src/Task/Task.ts
@@ -1,2 +1,14 @@
+import { TaskStatus } from './TaskStatus';
+
 /**任务 */
-export class Task {}
+export class Task {
+    /**任务状态 */
+    taskStatus: TaskStatus;
+    /**任务描述 */
+    description: string;
+
+    constructor({ description }: { description: string }) {
+        this.taskStatus = TaskStatus.Untouchable;
+        this.description = description;
+    }
+}
--- src/Task/TaskItem.ts
+++ src/Task/TaskItem.ts
@@ -1,11 +1,4 @@
-import { TaskStatus } from './TaskStatus';
-
 /**任务项 */
 export class TaskItem {
-    /**任务状态 */
-    taskStatus: TaskStatus;
-
-    constructor() {
-        this.taskStatus = TaskStatus.Untouchable;
-    }
+    // target:
 }
