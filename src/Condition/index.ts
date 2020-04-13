93eb5e3667c03f589f7b322fcde61b9f7426cbdb
vspirit803 <52484564+vspirit803@users.noreply.github.com>
2020-04-13 23:40:35 +0800

    refactor: modify export method

--- src/Condition/index.ts
+++ src/Condition/index.ts
@@ -1,4 +1,2 @@
-import { Condition, LogicOperator } from './Condition';
-import { ConditionItem } from './ConditionItem';
-
-export { Condition, ConditionItem, LogicOperator };
+export * from './Condition';
+export * from './ConditionItem';
