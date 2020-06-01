7bc2629f8526a583cae7617a8eb4da5f3f00d532
vspirit803 <52484564+vspirit803@users.noreply.github.com>
2020-06-01 17:45:19 +0800

    fix: fix bug when initializing  Character with configuration

--- src/Character/CharacterCenter.ts
+++ src/Character/CharacterCenter.ts
@@ -105,7 +105,7 @@ export class CharacterCenter implements SaveInterface<Array<CharacterSave>> {
             throw Error(`id为[${id}]的角色配置不存在`);
         }
         const character = new CharacterNormal({
-            equipmentSlots: commonEquipmentSlotsConfiguration,
+            // equipmentSlots: commonEquipmentSlotsConfiguration,
             ...characterConfiguration,
         });
         this.addCharacter(character);
